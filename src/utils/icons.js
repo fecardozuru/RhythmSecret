74ab0408b65363a24557ddf4b2f775cfd132457b
fecardozuru <fecardozuru@gmail.com>
2026-01-25 23:43:03 -0300

    Create icons.js

---
+++ src/utils/icons.js
@@ -0,0 +1,45 @@
+// /src/utils/icons.js
+// Ícones SVG inline para substituir bibliotecas externas
+
+export const Icons = {
+  // Music/Audio Icons
+  Music: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
+    </svg>
+  ),
+  
+  Play: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M8 5v14l11-7z"/>
+    </svg>
+  ),
+  
+  Pause: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+    </svg>
+  ),
+  
+  Volume: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+    </svg>
+  ),
+  
+  // Navigation Icons
+  Settings: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z"/>
+    </svg>
+  ),
+  
+  ChevronRight: () => (
+    <svg fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5">
+      <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+    </svg>
+  ),
+  
+  // Status Icons
+  Check: () => (
+    <svg fill="currentColor" viewBox="0 0
