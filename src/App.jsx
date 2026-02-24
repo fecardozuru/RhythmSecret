6642093d5059811d362960d459d9eb12a987ed9b
fecardozuru <fecardozuru@gmail.com>
2026-02-25 01:39:19 +0300

    feat: desativar login Google temporariamente, renderizar AppPage diretamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { AppProvider } from './contexts/AppContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { AppPage } from './pages';
 
+// Login com Google desativado temporariamente.
+// Renderiza AppPage diretamente sem autenticacao.
+function App() {
+    return (
+        <ThemeProvider>
+            <AppProvider>
+                <AppPage />
+            </AppProvider>
+        </ThemeProvider>
+    )
+}
+
+export default App;
